63cd05de2f391b59ac4ff562e36ccece86297d2f
AbdellaouiBen <benbillal@hotmail.com>
2020-05-13 11:25:30 +0200

    morning

--- app/Helpers/Calendar/Events.php
+++ app/Helpers/Calendar/Events.php
@@ -6,7 +6,10 @@ use App\Event;
 
 class Events {
 
-
+    /**
+     * recupere les evenements entre deux dates
+     * 
+     */
     public function getEventsBetween (\DateTime $start, \DateTime $end ): array{
         $sql = Event::whereBetween('start', array($start->format('Y-m-d 00:00:00'), $end->format('Y-m-d 23:59:59')) )->get();
         var_dump($sql);
