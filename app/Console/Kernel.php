f4783cdc7082417570507cbd14f160e734b63f33
AbdellaouiBen <benbillal@hotmail.com>
2020-05-16 00:35:37 +0200

    ??

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Event;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +27,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // $schedule->command('absenceAuto')->weekdays()->hourly()->when(function () {
+        //     return date('H') >= 8 && date('H') <= 17;
+        // });
+        // $schedule->command('absenceAuto')->everyFiveMinutes()->when(function () use ($dateInDatabase) {
+        //     return (
+
+        //         Event::where('end', new DateTime::format('Y-m-y H-i'))
+        //         $dateInDatabase == Carbon::today() ||
+        //         $dateInDatabase == Carbon::yesterday() ||
+        //         $dateInDatabase == Carbon::subDays(2)
+        //     );
+        // });
+        // $schedule->call(function () {
+        //     $events = Event::where('end', '=', Carbon::now('Y-m-d'))->where('end', '<', Carbon::now())->get();
+        //     $events->
+
+        // })->hourly();
     }
 
     /**
