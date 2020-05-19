2558d186915fd4705014c73e8ebf2bf1bc28b647
Ismael <ismaelazd@hotmail.com>
2020-05-19 15:20:19 +0200

    Fin messagerie

--- app/Messagerie.php
+++ app/Messagerie.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Messagerie extends Model
 {
-    //
+    public function ecrivain(){
+        return $this->belongsTo('App\User','ecrivain_id');   
+    }
 }
